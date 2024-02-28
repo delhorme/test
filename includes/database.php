75d2f94a234b066b8107ace9d4680e91a6eddec7
Oxyridium <delhormelaurent@yahoo.fr>
2024-02-28 16:43:10 +0100

    Add files via upload

--- includes/database.php
+++ includes/database.php
@@ -1,8 +1,8 @@
 <?php
 define('HOST','localhost');
-define('DB_NAME','siteweb');
-define('USER','root');
-define('PASS','');
+define('DB_NAME','u789471193_siteweb');
+define('USER','u789471193_root');
+define('PASS','12756428Ld');
 
 try{
     $db = new PDO("mysql:host=" . HOST . ";dbname=" . DB_NAME, USER, PASS);
