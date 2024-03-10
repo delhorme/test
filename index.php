dcc358cf295c2f7d325ad28f41e63a14f2b3ce52
Oxyridium <delhormelaurent@yahoo.fr>
2024-03-10 22:35:26 +0100

    ok

--- index.php
+++ index.php
@@ -7,7 +7,7 @@
 
 <body>
 
-    <h1>Bienvenue sur votre profil !</h1>
+    <!-- <h1>Bienvenue sur votre profil !</h1> -->
     <?php
 
     if (isset($_SESSION['email']) && (isset($_SESSION['password']))) {
