4b257af75ddfeca4dd698d1a07d5163afe019f6c
Oxyridium <delhormelaurent@yahoo.fr>
2024-03-25 19:23:43 +0100

    ok

--- includes/login.php
+++ includes/login.php
@@ -18,6 +18,7 @@ if (isset($_POST['formlogin'])) {
                 echo "Le mot de passe est bon, connexion en cours";
                 $_SESSION['email'] = $result['email'];
                 $_SESSION['date'] = $result['date'];
+                window.location.replace("http://premiere.php/");
             }
            else
            {
